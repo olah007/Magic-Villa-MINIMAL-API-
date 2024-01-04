a1e72e9a224f6140f3cc4b2677cb5839d2fa0e7c
Taofeek Issa <taofeek.issa@sevenup.org>
2024-01-04 19:00:42 +0100

    completed basic CRUD operations

--- MinimalAPIDemo/MagicVilla_CouponAPI/Program.cs
+++ MinimalAPIDemo/MagicVilla_CouponAPI/Program.cs
@@ -4,6 +4,7 @@ using MagicVilla_CouponAPI;
 using MagicVilla_CouponAPI.Data;
 using MagicVilla_CouponAPI.Models;
 using MagicVilla_CouponAPI.Models.DTO;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
 using System.Net;
@@ -67,24 +68,24 @@ app.MapGet("/api/coupon/{id:int}", (ILogger < Program > _logger, int id) =>
     return Results.Ok(response);
 }).WithName("GetCoupon").Produces<APIResponse>(200);
 
-app.MapPost("/api/coupon", async ([FromBody] CouponCreateDTO couponCreateDTO, IMapper _mapper, IValidator<CouponCreateDTO> _validation) =>
+app.MapPost("/api/coupon", async ([FromBody] CouponCreateDTO coupon_C_DTO, IMapper _mapper, IValidator<CouponCreateDTO> _validation) =>
 {
     APIResponse response = new() { IsSuccess = false, StatusCode = HttpStatusCode.BadRequest };
     
     //var validationResult = _validation.ValidateAsync(couponCreateDTO).GetAwaiter().GetResult();
-    var validationResult = await _validation.ValidateAsync(couponCreateDTO);
+    var validationResult = await _validation.ValidateAsync(coupon_C_DTO);
     if (!validationResult.IsValid)
     {
         response.ErrorMessages.Add(validationResult.Errors.FirstOrDefault().ToString());
         return Results.BadRequest(response);
     }
-    if (CouponStore.couponList.FirstOrDefault(x => x.Name.ToLower() == couponCreateDTO.Name.ToLower()) != null)
+    if (CouponStore.couponList.FirstOrDefault(x => x.Name.ToLower() == coupon_C_DTO.Name.ToLower()) != null)
     {
         response.ErrorMessages.Add("Coupon Name already Exists.");
         return Results.BadRequest(response);
     }
     
-    Coupon coupon = _mapper.Map<Coupon>(couponCreateDTO);
+    Coupon coupon = _mapper.Map<Coupon>(coupon_C_DTO);
     coupon.Id = CouponStore.couponList.OrderBy(x => x.Id).ToList()[^1].Id + 1;
     CouponStore.couponList.Add(coupon);
     CouponDTO couponDTO = _mapper.Map<CouponDTO>(coupon);
@@ -97,6 +98,55 @@ app.MapPost("/api/coupon", async ([FromBody] CouponCreateDTO couponCreateDTO, IM
     //return Results.Created($"/api/coupon/{coupon.Id}", coupon);
     //return Results.Ok(coupon);
 }).WithName("CreateCoupon").Accepts<CouponCreateDTO>("application/json").Produces<APIResponse>(201).Produces(400);
+
+app.MapPut("api/coupon", async ([FromBody] CouponUpdateDTO coupon_U_DTO, IMapper _mapper, IValidator<CouponUpdateDTO> _validation) =>
+{
+    APIResponse response = new() { IsSuccess = false, StatusCode = HttpStatusCode.BadRequest };
+
+    var validationResult = await _validation.ValidateAsync(coupon_U_DTO);
+    if (!validationResult.IsValid)
+    {
+        response.ErrorMessages.Add(validationResult.Errors.FirstOrDefault().ToString());
+        return Results.BadRequest(response);
+    }
+    
+    Coupon couponFromStore = CouponStore.couponList.FirstOrDefault(u => u.Id == coupon_U_DTO.Id);
+    if(couponFromStore != null)
+    {
+        couponFromStore.IsActive = coupon_U_DTO.IsActive;
+        couponFromStore.Name = coupon_U_DTO.Name;
+        couponFromStore.Percent = coupon_U_DTO.Percent;
+        couponFromStore.LastUpdated = DateTime.Now;
+    }
+    else
+    {
+        response.ErrorMessages.Add("Invalid id");
+        return Results.BadRequest(response);
+    }
+    
+    response.Result = _mapper.Map<CouponDTO>(couponFromStore);
+    response.IsSuccess = true;
+    response.StatusCode = HttpStatusCode.OK;
+    return Results.Ok(response);
+}).WithName("UpdateCoupon").Accepts<CouponUpdateDTO>("application/json").Produces<APIResponse>(200).Produces(400);
+
+app.MapDelete("api/coupon/{id:int}", (int id) =>
+{
+    APIResponse response = new() { IsSuccess = false, StatusCode = HttpStatusCode.BadRequest };
+
+    Coupon couponFromStore = CouponStore.couponList.FirstOrDefault(u => u.Id == id);
+    if (couponFromStore != null)
+        CouponStore.couponList.Remove(couponFromStore);
+    else
+    {
+        response.ErrorMessages.Add("Invalid id");
+        return Results.BadRequest(response);
+    }
+
+    response.IsSuccess = true;
+    response.StatusCode = HttpStatusCode.NoContent;
+    return Results.Ok(response);
+});
 
 app.UseHttpsRedirection();
 
