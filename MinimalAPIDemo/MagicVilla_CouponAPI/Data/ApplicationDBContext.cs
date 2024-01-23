451c1cd101983486c7b14de7399dfce5bb9fd64a
Taofeek Issa <taofeek.issa@sevenup.org>
2024-01-23 17:52:13 +0100

    Added Application DB-Context

---
+++ MinimalAPIDemo/MagicVilla_CouponAPI/Data/ApplicationDBContext.cs
@@ -0,0 +1,35 @@
+﻿using MagicVilla_CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_CouponAPI.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Coupon>().HasData(
+                new Coupon()
+                {
+                    Id = 1,
+                    Name = "100FF",
+                    Percent = 10,
+                    IsActive = true,
+                },
+                new Coupon()
+                {
+                    Id = 2,
+                    Name = "200FF",
+                    Percent = 20,
+                    IsActive = true,
+                }
+            );
+        }
+    }
+}
