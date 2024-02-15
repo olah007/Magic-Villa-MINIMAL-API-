4c1ace1b4c969b2f1ecada764556f93026561e14
Taofeek Issa <taofeek.issa@sevenup.org>
2024-02-15 09:18:03 +0100

    started Authentication and Authorization

---
+++ MinimalAPIDemo/MagicVilla_CouponAPI/Models/DTO/LoginRequestDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MagicVilla_CouponAPI.Models.DTO
+{
+    public class LoginRequestDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+    }
+}
